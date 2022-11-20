b44248a7cbb2ec2acdb4adbe813a3d1043f718e5
Petersonk4 <118634732+Petersonk4@users.noreply.github.com>
2022-11-19 20:00:37 -0600

    Update cal.py

--- cal.py
+++ cal.py
@@ -22,15 +22,15 @@ While True:
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
             
-            elif choice == '2':
-                print(num1, "-", num2, "=", subtract(num1, num2))
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
                 
                 
-                elif choice == '3':
-                    print(num1, "*", num2, "=", multilpy(num1, num2))
+        elif choice == '3':
+            print(num1, "*", num2, "=", multilpy(num1, num2))
                 
-                elif choice == '4':
-                    print(num1, "/", num2, "=", divide(num1, num2))
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
                     
 next_calculation = input("Let's do next calculation? (Yes/No): ")
 if next_calculation == "no":
